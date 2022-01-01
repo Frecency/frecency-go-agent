0b86f37dc4d71fd2642ef7140d869d86f03c052e
Frecency <132741843+Frecency@users.noreply.github.com>
2022-01-01 17:30:46 +0000

    v5.2

--- client/config/parameters.go
+++ client/config/parameters.go
@@ -10,4 +10,9 @@ package config
 var Sleeptime = 5
 
 // Jitter is random extra delay to be added to the sleeptime
-var Jitter = 
+var Jitter = 5
+
+// Retries is the amount of tries to keep trying in case C2 is unreachable
+var Retries = 3
+
+// UserAgent is user agent that
