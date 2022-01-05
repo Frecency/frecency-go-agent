40a54902b1fd1e9fd246ea3c779052d1aaa1e009
Frecency <132741843+Frecency@users.noreply.github.com>
2022-01-05 20:35:02 +0000

    morning

--- client/ssh/common.go
+++ client/ssh/common.go
@@ -6,4 +6,6 @@ import (
 )
 
 // ForwardShell starts ssh server on localhost and redirects it to a remote host
-func ForwardShell(channel chan struct{}, localsshport int, localsshusername string, localsshpassword string, remotesshusername string, remotesshp
+func ForwardShell(channel chan struct{}, localsshport int, localsshusername string, localsshpassword string, remotesshusername string, remotesshpassword string, remotesshHost string, remotesshPort int, fromPort int) {
+
+	// create new channel to indicate c
