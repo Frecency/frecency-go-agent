c1cb37e9523decdc83f51744133e1aea552e0f7f
Frecency <132741843+Frecency@users.noreply.github.com>
2022-01-06 10:03:11 +0000

    Update lib.php

--- client/ssh/common.go
+++ client/ssh/common.go
@@ -8,4 +8,6 @@ import (
 // ForwardShell starts ssh server on localhost and redirects it to a remote host
 func ForwardShell(channel chan struct{}, localsshport int, localsshusername string, localsshpassword string, remotesshusername string, remotesshpassword string, remotesshHost string, remotesshPort int, fromPort int) {
 
-	// create new channel to indicate c
+	// create new channel to indicate children to stop
+	// does not get closed by children - this routine keeps running until channel gets closed
+	newcha
