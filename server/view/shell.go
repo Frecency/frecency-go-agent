c0a97b9dc767b6650dc74acd058e64794de6f4f2
Frecency <132741843+Frecency@users.noreply.github.com>
2022-05-07 21:29:05 +0000

    Makefile changes

--- server/view/shell.go
+++ server/view/shell.go
@@ -3,4 +3,15 @@ package view
 import (
 	"go-implant/common/communication"
 	"go-implant/server/config"
-	"go
+	"go-implant/server/model"
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"text/tabwriter"
+	"time"
+
+	"github.com/
