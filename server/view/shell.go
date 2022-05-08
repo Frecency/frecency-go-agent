a5e9b22367dc6ad9f6215fa182121a5219803bca
Frecency <132741843+Frecency@users.noreply.github.com>
2022-05-08 22:22:21 +0000

    Renaming variables

--- server/view/shell.go
+++ server/view/shell.go
@@ -25,4 +25,9 @@ func init() {
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-func randStringRunes(n 
+func randStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	r
